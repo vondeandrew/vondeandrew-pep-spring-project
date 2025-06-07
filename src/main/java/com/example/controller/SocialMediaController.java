5204b08a8193dbbd3ea73ba1036c5aa31141baea
Andrew Vonderhaar <vondeandrew@gmail.com>
2025-06-07 14:48:46 +0000

    commit start

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,19 @@
 package com.example.controller;
 
+import javax.websocket.server.PathParam;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import antlr.collections.List;
+import ch.qos.logback.core.joran.spi.DefaultClass;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +21,49 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
+    @GetMapping("/register")
+    public ResponseEntity<Account> register(@RequestBody Account account) {
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Account> login(@RequestBody Account loginInfo)
+    {
+
+    }
+
+    @PostMapping("/messages")
+    public ResponseEntity<Message> creatMessage(@RequestBody Message message)
+    {
+
+    }
+
+    @GetMapping("/messages")
+    public ResponseEntity<List<Message>> getAllMessages() {
+
+    }
+
+    @GetMapping("/messages/{messageId}")
+    public ResponseEntity<Message> getmessageByID(@PathVariable int messageId)
+    {
+
+    }
+
+    @DeleteMapping("/messages/{messageId}")
+    public ResponseEntity<Message> deleteMessage(@PathVariable int messageId)
+    {
+
+    }
+
+    @PatchMapping("/messages/{messageId}")
+    public ResponseEntity<Message> updateMessage (@PathVariable int messageId, @RequestBody Message newMessage)
+    {
+
+    }
+
+    @getMapping("/accounts/{accountIs}/messages")
+    public
 }
